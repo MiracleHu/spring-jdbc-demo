cbb3a2eb5de2c28ef687922ab41cb57e593b8b99
Huanle Hu <huanlehu@Huanles-MacBook-Pro.local>
2018-05-05 00:25:55 +0900

    bean

--- src/main/java/com/yiibai/common/App.java
+++ src/main/java/com/yiibai/common/App.java
@@ -12,6 +12,7 @@ public class App
     	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
     	
     	//接口由Spring-Customer.xml bean配置好了就能当做实例化好的对象来使用了 
+        //context相当于map，里面存了好多预先初始化好的Object
         CustomerDAO customerDAO = (CustomerDAO) context.getBean("customerDAO");
         Customer customer = new Customer(1, "yiibai",29);
         customerDAO.insert(customer);
